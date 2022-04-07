6ded9ba9ccb64776b89fd639b626c9506fc78ae9
hiren polara <hiren.polara22@gmail.com>
2022-04-07 15:19:34 +0530

    about us page ui design start / footer slides change

--- src/js/parts/app.js
+++ src/js/parts/app.js
@@ -76,27 +76,20 @@ export class App{
     arrows: false,
     autoplay: true,
     dots: false,
-    responsive: [{
-      breakpoint: 1200,
-      settings: {
-        slidesToShow: 5
-      }
-    }, {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 4
-      }
-    }, {
-      breakpoint: 767,
-      settings: {
-        slidesToShow: 3
-      }
-    }, {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 3
-      }
-    }]
+    responsive: [
+      {
+        breakpoint: 1400,
+        settings: {
+          slidesToShow: 4
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+    ]
   });
 
   $('.testimonial-carousel').slick({
